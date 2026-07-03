0702c4bbf60072e8d132a51e3c51700a2b5b7913
skc4365 <50658047+skc4365@users.noreply.github.com>
2026-07-03 17:55:18 +0900

    edit App경로수정-BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/thein-reactApp">
     <App />
   </BrowserRouter>,
 );
